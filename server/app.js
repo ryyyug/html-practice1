681f6202150bc671006487121ddc66ee2f93c2e5
ryyyug <86996629+ryyyug@users.noreply.github.com>
2021-07-20 12:46:42 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require("express")
+const path = require("path")
+const app = express()
+const port = 3000
+
+// app.get("/", (req, res) => {
+//   res.send("Hello")
+// })
+app.use("/html-practice1", express.static(path.resolve(__dirname, "../docs")))
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
